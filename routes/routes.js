fbe2dcb9de9f9cf5df7a8bbb50265b0bcea322f8
Dylan James <djjames455@gmail.com>
2022-03-06 15:32:15 -0500

    Add routes, config, and index.html file

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,45 @@
+const pool = require('../data/config');
+
+const users = [{
+    id: 1,
+    name: "Instructor",
+    email: "instructor_345@stevens.edu"
+},
+{
+    id: 2,
+    name: "TA",
+    email: "ta_345@stevens.edu",
+}
+];
+
 const router = app => {
     app.get('/', (request, response) => {
         console.log(`URL: ${request.url}`);
         response.send({ message: 'Node.js and Express REST API!'});
-    });    
+    });   
+    
+    app.get('/users', (request, response) => {
+        response.send(users);
+    });
+    
+    // Display workers by using DB query
+    app.get('/handles', (request, response) => {
+        pool.query('SELECT * FROM Handle', (error, result) => {
+            if (error) throw error;
+
+            response.send(result);
+        });
+    });
+
+    // Display workers by using DB query
+    app.get('/handles/:handle', (request, response) => {
+        const handle = request.params.handle;
+        pool.query('SELECT * FROM Handle WHERE handle=?', handle, (error, result) => {
+            if (error) throw error;
+
+            response.send(result);
+        });
+    });
 };
 
 module.exports = router;
